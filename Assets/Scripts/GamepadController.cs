8d492b618d56fa5981cafb9f437bed4855665548
mjbogusz <mjbogusz+github@gmail.com>
2018-06-02 19:41:33 +0200

    Added more inputs, pointer object and pointer controller script

--- Assets/Scripts/GamepadController.cs
+++ Assets/Scripts/GamepadController.cs
@@ -4,40 +4,48 @@ using UnityEngine;
 
 public class GamepadController : MonoBehaviour {
 	private static readonly float maxSpeed = 1.0f;
-	private static readonly float acceleration = 2.0f;
+	private static readonly float acceleration = 3.0f * Time.fixedDeltaTime;
 	private float speedX = 0f;
 	private float speedZ = 0f;
+	private bool movingMode = true;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
-		float vertical = Input.GetAxis("Vertical");
-		float horizontal = Input.GetAxis("Horizontal");
-
-		float speedDiffX = horizontal - speedX;
-		if (speedDiffX > acceleration * Time.fixedDeltaTime) {
-			speedX += acceleration * Time.fixedDeltaTime;
-		} else if (speedDiffX < -acceleration * Time.fixedDeltaTime) {
-			speedX -= acceleration * Time.fixedDeltaTime;
-		} else {
-			speedX += speedDiffX;
+		if (Input.GetButtonUp("Fire3")) {
+			movingMode = !movingMode;
 		}
 
-		float speedDiffZ = vertical - speedZ;
-		if (speedDiffZ > acceleration * Time.fixedDeltaTime) {
-			speedZ += acceleration * Time.fixedDeltaTime;
-		} else if (speedDiffZ < -acceleration * Time.fixedDeltaTime) {
-			speedZ -= acceleration * Time.fixedDeltaTime;
+		if (movingMode) {
+			float vertical = Input.GetAxis("Vertical");
+			float horizontal = Input.GetAxis("Horizontal");
+
+			float speedDiffX = horizontal - speedX;
+			if (speedDiffX > acceleration) {
+				speedX += acceleration;
+			} else if (speedDiffX < -acceleration) {
+				speedX -= acceleration;
+			} else {
+				speedX += speedDiffX;
+			}
+
+			float speedDiffZ = vertical - speedZ;
+			if (speedDiffZ > acceleration) {
+				speedZ += acceleration;
+			} else if (speedDiffZ < -acceleration) {
+				speedZ -= acceleration;
+			} else {
+				speedZ += speedDiffZ;
+			}
 		} else {
-			speedZ += speedDiffZ;
+			speedX = 0;
+			speedZ = 0;
 		}
 
 		Vector3 translationVector = new Vector3(speedX, 0f, speedZ) * Time.fixedDeltaTime * maxSpeed;
-		translationVector = Quaternion.Euler(transform.Find("Main Camera").eulerAngles) * translationVector;
+		translationVector = Quaternion.Euler(transform.Find("mainCamera").eulerAngles) * translationVector;
 		transform.Translate(translationVector);
 	}
 }
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour {
+	private static readonly float maxSpeed = 20f;
+	private static readonly float distanceAcceleration = 5f * Time.fixedDeltaTime;
+
+	private bool aimingMode = false;
+	private float angle = 0f;
+	private float height = 1f;
+	private float distance = 1f;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		// Toggle aiming/movement mode
+		if (Input.GetButtonUp("Fire4")) {
+			aimingMode = !aimingMode;
+		}
+
+		// Display data readouts
+		if (Input.GetButton("Fire0")) {
+			///TODO: display readouts
+		}
+
+		if (!aimingMode) {
+			return;
+		}
+
+		// Reset pointer position
+		if (Input.GetButtonUp("Fire3")) {
+			angle = 0f;
+			height = 1f;
+			distance = 1f;
+		}
+
+		// vertical (up/down): move up/down
+		float vertical = Input.GetAxis("Vertical");
+		// horizontal (left/right): rotate around camera
+		float horizontal = Input.GetAxis("Horizontal");
+		float distanceChange = 0f;
+		if (Input.GetButton("Fire1")) {
+			distanceChange += 1f;
+		}
+		if (Input.GetButton("Fire2")) {
+			distanceChange -= 1f;
+		}
+
+		///TODO: find player position and camera angles
+		///TODO: apply changes
+	}
+}
