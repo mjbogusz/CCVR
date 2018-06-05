fdd1e91f24cffdfbddc328a47a9799449d827c48
mjbogusz <mjbogusz+github@gmail.com>
2018-06-05 14:20:52 +0200

    Fixed movement controls; added inputs for more gamepad buttongs

--- Assets/Scripts/GamepadController.cs
+++ Assets/Scripts/GamepadController.cs
@@ -3,52 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GamepadController : MonoBehaviour {
-	private float maxSpeed = 1.0f;
-	private float acceleration = 3.0f;
-	private float speedX = 0f;
-	private float speedZ = 0f;
+	private float speedMax = 5.0f;
+	private float timeToFullSpeed = 0.5f;
+	private float accelerationMax = 3.0f;
+	//private float accelerationX = 0f;
+	//private float accelerationY = 0f;
+	private Vector3 speed = new Vector3(0f, 0f, 0f);
 	private bool movingMode = true;
 
 	// Use this for initialization
-	void Start () {
-		maxSpeed *= Time.fixedDeltaTime;
-		acceleration *= Time.fixedDeltaTime;
+	void Start() {
+		speedMax *= Time.fixedDeltaTime;
+		accelerationMax *= Time.fixedDeltaTime;
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if (Input.GetButtonUp("Fire3")) {
 			movingMode = !movingMode;
 		}
+
+		Vector3 speedTarget = new Vector3(0f, 0f, 0f);
 
 		if (movingMode) {
 			float vertical = Input.GetAxis("Vertical");
 			float horizontal = Input.GetAxis("Horizontal");
 
-			float speedDiffX = horizontal - speedX;
-			if (speedDiffX > acceleration) {
-				speedX += acceleration;
-			} else if (speedDiffX < -acceleration) {
-				speedX -= acceleration;
-			} else {
-				speedX += speedDiffX;
-			}
-
-			float speedDiffZ = vertical - speedZ;
-			if (speedDiffZ > acceleration) {
-				speedZ += acceleration;
-			} else if (speedDiffZ < -acceleration) {
-				speedZ -= acceleration;
-			} else {
-				speedZ += speedDiffZ;
-			}
-		} else {
-			speedX = 0;
-			speedZ = 0;
+			float cameraAngle = transform.Find("mainCamera").eulerAngles.y;
+			speedTarget = Quaternion.Euler(0, cameraAngle, 0) * new Vector3(horizontal, 0, vertical);
 		}
 
-		Vector3 translationVector = new Vector3(speedX, 0f, speedZ) * Time.fixedDeltaTime * maxSpeed;
-		translationVector = Quaternion.Euler(transform.Find("mainCamera").eulerAngles) * translationVector;
-		transform.Translate(translationVector);
+		Vector3 acceleration = (speedTarget - speed) * Time.fixedDeltaTime;
+		if (acceleration.magnitude > accelerationMax) {
+			acceleration *= (accelerationMax / acceleration.magnitude);
+		}
+
+		speed += acceleration;
+		transform.Translate(speed * speedMax);
 	}
 }
